35e06cd23d8e462b42e1734f1d9cacf83b8ede01
Aurimas Urbonas <aurimas@smartpartner.lt>
2025-09-25 19:23:18 +0300

    Trait ORM attributes

--- src/Entity/VendorsTrait.php
+++ src/Entity/VendorsTrait.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace Odiseo\SyliusVendorPlugin\Entity;
 
+
+use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 
 trait VendorsTrait
 {
+    #[ORM\ManyToMany(targetEntity: VendorInterface::class)]
+    #[ORM\JoinTable(name: 'odiseo_vendor_channels' ]
+    #[ORM\JoinColumn(name: 'channel_id', referencedColumnName: 'id')]
+    #[ORM\InverseJoinColumn(name: 'vendor_id', referencedColumnName: 'id')]
     protected Collection $vendors;
 
     public function __construct()
